af080f09c89bae2e8fd49606eab3dbf4473e2934
theorlangur <theorlangur@gmail.com>
2020-09-02 21:45:26 +0200

    added helping trait LineParser for easier parsing (white-spaces vs non-white-spaces)

--- src/analyzers.rs
+++ src/analyzers.rs
@@ -3,6 +3,7 @@ use crate::column_tools::LineDescr;
 
 pub mod separators;
 pub mod assignment;
+pub mod func_decl;
 
 #[derive(Debug)]
 pub struct AnalyzeErr{
@@ -22,4 +23,31 @@ pub trait LineAnalyzer
 {
     fn clear(&mut self){}
     fn analyze_line<'a>(&mut self, _fmt :&mut Formatter, _l: &mut LineDescr<'a>)->Result<(),AnalyzeErr>{Err(AnalyzeErr{})}
+}
+
+pub trait LineParser
+{
+    fn rfind_nwhite(&self)->Result<usize, AnalyzeErr>;
+    fn rfind_white(&self)->Result<usize, AnalyzeErr>;
+    fn find_nwhite(&self)->Result<usize, AnalyzeErr>;
+    fn find_white(&self)->Result<usize, AnalyzeErr>;
+    fn sym(&self, c: char)->Result<usize, AnalyzeErr>;
+}
+
+impl LineParser for str {
+    fn rfind_nwhite(&self)->Result<usize, AnalyzeErr> {
+        self.rfind(|c:char|!c.is_ascii_whitespace()).ok_or(AnalyzeErr{})
+    }
+    fn rfind_white(&self)->Result<usize, AnalyzeErr> {
+        self.rfind(|c:char|c.is_ascii_whitespace()).ok_or(AnalyzeErr{})
+    }
+    fn find_nwhite(&self)->Result<usize, AnalyzeErr>{
+        self.find(|c:char|!c.is_ascii_whitespace()).ok_or(AnalyzeErr{})
+    }
+    fn find_white(&self)->Result<usize, AnalyzeErr> {
+        self.find(|c:char|c.is_ascii_whitespace()).ok_or(AnalyzeErr{})
+    }
+    fn sym(&self, c: char)->Result<usize, AnalyzeErr> {
+        self.find(c).ok_or(AnalyzeErr{})
+    }
 }
